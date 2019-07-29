1bf24b1eb62e3cc1b36dbfd2be38a562fd1a501b
yangzhongming <yzhming@sina.cn>
2019-07-29 13:35:11 +0800

    init

---
+++ edu-cloud-parent/src/main/java/com/cn/edu/parent/manage/App.java
@@ -0,0 +1,16 @@
+package com.cn.edu.parent.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:Alex
+ * @date:2019/7/29
+ * @description:
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
